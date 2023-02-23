4851ced82ee16e5b9e3f0a59cc9da845f0f1d230
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-02-22 19:49:17 -0600

    safsf

--- dist/MPrestamo.php
+++ dist/MPrestamo.php
@@ -31,7 +31,7 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="card mb-4">
-                            <div class="card-header"><strong>Prestamo de Activo</strong></div>
+                            <div class="card-header"><strong>Movimiento de Bienes Muebles</strong></div>
                             <div class="card-body">
                                 <!--INICIO FORM-->
                                 <form class="g-3 needs-validation" novalidate="">
@@ -94,6 +94,20 @@
                                     <!--FIN-->
                                     <h4>Caracteristicas</h4>
                                     <hr style="color: black; background-color: black; width:100%;" />
+                                    <div class="row my-4">
+                                        <div class="col-md-2">
+                                            <label class="form-label" for="validationCustom01">Nombre:</label>
+                                            <input class="form-control" id="validationCustom01" type="date" required="">
+                                        </div>
+                                        <div class="col-md-3">
+                                            <label class="form-label" for="validationCustom02">Descripcion:</label>
+                                            <input class="form-control" id="validationCustom02" type="text" required="">
+                                        </div>
+                                        <div class="col-md-3">
+                                            <label class="form-label" for="validationCustom02">modelo:</label>
+                                            <input class="form-control" id="validationCustom02" type="text" required="">
+                                        </div>
+                                    </div>
                                     <!--FIN-->
                                     <div class="col-12" align="right">
                                         <button class="btn btn-primary" type="submit">Guardar</button>
