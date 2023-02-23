3d59c087f56d85a7dab942a71b010f659052c800
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-02-22 19:31:57 -0600

    fadsf

--- dist/MPrestamo.php
+++ dist/MPrestamo.php
@@ -67,6 +67,12 @@
                                             </textarea>
                                         </div>
                                     </div>
+                                    <div class="form-check">
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+  <label class="form-check-label" for="flexRadioDefault1">
+    Default radio
+  </label>
+</div>
                                     
                                     <!--FIN-->
                                     
