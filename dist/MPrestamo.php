47afc1fc323679a0ad9357439cfa8d32f115ed51
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-02-22 19:38:50 -0600

    dsfds

--- dist/MPrestamo.php
+++ dist/MPrestamo.php
@@ -68,8 +68,7 @@
                                         </div>
                                     </div>
                                     <div class="row my-2 ">
-                                    <div class="col-md-3">
-                                        <div class="form-check">
+                                        <div class="form-check col-md-3">
                                             <input class="form-check-input" type="radio" name="flexRadioDefault"
                                                 id="flexRadioDefault1">
                                             <label class="form-check-label" for="flexRadioDefault1">
@@ -89,7 +88,6 @@
                                             <label class="form-check-label" for="flexRadioDefault1">
                                                 Reparación
                                             </label>
-                                        </div>
                                         </div>
                                     </div>
                                     <!--FIN-->
