b55fb28b8ae2fe92349b67d6508a2b449467d31c
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-02-27 18:38:09 -0600

    sfsdf

--- dist/ARequisicion.php
+++ dist/ARequisicion.php
@@ -70,7 +70,7 @@
                       <label for="inputCity" class="form-label">Cantidad:</label>
                       <input type="number" class="form-control" id="inputCity">
                     </div>
-                    <div class="col-md-1">
+                    <div class="col-md-0">
                     <button class="btn btn-primary" type="submit">Agregar</button>
                     </div>
                   </div>
