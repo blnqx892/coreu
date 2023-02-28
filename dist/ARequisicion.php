396861d4d7522f366fa134f37581ad2037e5bd7b
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-02-27 18:41:06 -0600

    fgdfgdf

--- dist/ARequisicion.php
+++ dist/ARequisicion.php
@@ -71,9 +71,7 @@
                       <input type="number" class="form-control" id="inputCity">
                     </div>
                     <div class="col-md-1">
-                    </div>
-                    <div class="col-md-1">
-                    <label for="inputCity" class="form-label">:</label>
+                    <label for="inputCity" class="form-label"></label>
                     <button class="btn btn-primary" type="submit">+</button>
                     </div>
                   </div>
