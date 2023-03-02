2d5a7b38f55050336a74ced22dcd5439d7c49fb3
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-01 21:13:56 -0600

    fdgdfg

--- dist/menu/hori.php
+++ dist/menu/hori.php
@@ -59,12 +59,12 @@
           <svg class="icon me-2">
             <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-file"></use>
           </svg> Projects<span class="badge badge-sm bg-primary ms-2">42</span></a>
-        <div class="dropdown-divider"></div><a class="dropdown-item" href="#">
+        <div class="dropdown-divider"></div><a class="dropdown-item" href="Acceso.php">
           <svg class="icon me-2">
             <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
           </svg> Bloquear</a><a class="dropdown-item" href="#">
           <svg class="icon me-2">
-            <use xlink:href="Acceso.php"></use>
+            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
           </svg> Cerrar Sesión</a>
       </div>
     </li>
