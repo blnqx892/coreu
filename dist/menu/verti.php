0fe17a7aff98bd1230c7be1f985861db3c852d10
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-06 14:18:16 -0600

    configurando m enu

--- dist/menu/verti.php
+++ dist/menu/verti.php
@@ -1,12 +1,4 @@
-<div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
-  <div class="sidebar-brand d-none d-md-flex">
-    <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-      <use xlink:href="assets/brand/coreui.svg#full"></use>
-    </svg>
-    <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
-      <use xlink:href="assets/brand/coreui.svg#signet"></use>
-    </svg>
-  </div>
+
   <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
     <!--separador-->
     <li class="nav-title">ACTIVO FIJO</li>
@@ -206,4 +198,4 @@
     </li>
   </ul>
   <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
-</div>
+
