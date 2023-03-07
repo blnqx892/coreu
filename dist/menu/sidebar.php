4e5bf45557598e9538d8cdaa7907801107def885
Natalie <46360156+natalie72@users.noreply.github.com>
2023-03-07 16:10:43 -0600

    actualizacion menu

--- dist/menu/sidebar.php
+++ dist/menu/sidebar.php
@@ -83,7 +83,7 @@
             Ingreso Suministros</a></li>
       </ul>
       <ul class="nav-group-items">
-        <li class="nav-item"><a class="nav-link" href="CatalogoSuministros.php"><span class="nav-icon"></span>
+        <li class="nav-item"><a class="nav-link" href="catalogoSuministros.php"><span class="nav-icon"></span>
             Catalogo Suministros</a></li>
       </ul>
     </li>
