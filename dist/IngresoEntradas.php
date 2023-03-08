37db7a20ea11fc9c51e1e405616ff149673e5c7b
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-08 15:34:02 -0600

    rfsdf

--- dist/IngresoEntradas.php
+++ dist/IngresoEntradas.php
@@ -104,7 +104,7 @@
                         <textarea class="form-control" id="validationCustom02" required="" row="3">
                       </textarea></div>
                     </div>
-                    <div class="row  my-8">
+                    <div class="row  my-4"><br><br>
                       <div class="col-md-2">
                         <label class="form-check-label" for="flexSwitchCheckChecked">Transporte</label>
                         <div class="form-check form-switch">
