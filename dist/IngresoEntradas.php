3a15737e9cd48f63721ea3450a188d94ec873403
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-08 15:20:06 -0600

    dasf

--- dist/IngresoEntradas.php
+++ dist/IngresoEntradas.php
@@ -98,7 +98,7 @@
                     </div>
                   </div>
                   <!--FIN SECCION CUATRO-->
-                  <div class="row  my-3">
+                  <div class="row  my-8">
                     <!--INICIO SECCION CINCO-->
                     <div class="col-md-5">
                       <label for="inputZip" class="form-label">Descripción:</label>
