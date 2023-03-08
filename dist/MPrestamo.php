beab894ddc900a90acda9dcf0e77fd567021fcdf
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-08 15:56:26 -0600

    fdhfgh

--- dist/MPrestamo.php
+++ dist/MPrestamo.php
@@ -82,7 +82,7 @@
                         </label>
                       </div>
                     </div>
-                    <div class="row my-6">
+                    <div class="row my-4">
                       <div class="col-md-6">
                         <label class="form-label" for="validationCustom02">Obervaciones:</label>
                         <textarea class="form-control" id="validationCustom02" required="" row="3">
