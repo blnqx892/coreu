b4b683352c051854a01cff13ac721bc027784cb4
Natalie <46360156+natalie72@users.noreply.github.com>
2023-03-08 10:49:03 -0600

    nuevo

--- dist/menu/sidebar.php
+++ dist/menu/sidebar.php
@@ -83,7 +83,7 @@
             Ingreso Suministros</a></li>
       </ul>
       <ul class="nav-group-items">
-        <li class="nav-item"><a class="nav-link" href="actualizaionsuministros.php"><span class="nav-icon"></span>
+        <li class="nav-item"><a class="nav-link" href="TablaSumi.php"><span class="nav-icon"></span>
             Catalogo Suministros</a></li>
       </ul>
     </li>
