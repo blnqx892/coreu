ada9c36f64166a85e0a33ea11559b2f9c133e6df
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-08 14:45:18 -0600

    agregando vehiculo

--- dist/IngresoEntradas.php
+++ dist/IngresoEntradas.php
@@ -105,12 +105,13 @@
                       <textarea class="form-control" id="validationCustom02" required="" row="3">
                       </textarea>
                     </div>
-                  </div>
-                  <div class="row  my-4">
-                  <div class="form-check form-switch">
+                    <div class="form-check form-switch">
                       <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
                       <label class="form-check-label" for="flexSwitchCheckChecked">Vehiculo</label>
                     </div>
+                  </div>
+                  <div class="row  my-4">
+                  
                   </div>
                   <!--FIN SECCION CUATRO-->
                   <div class="col-15" align="right">
