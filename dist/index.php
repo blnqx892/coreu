9a1713fa67d3c1b3d692e4754103a151989e811c
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-09 09:26:01 -0600

    tamaño

--- dist/index.php
+++ dist/index.php
@@ -80,7 +80,7 @@
                 </div>
               </div>
               <!-- /.col-->
-              <div class="col-6 col-lg-3">
+              <div class="col-6 col-lg-4">
                 <div class="card overflow-hidden">
                   <div class="card-body p-0 d-flex align-items-center"><br>
                     <div>
@@ -125,7 +125,6 @@
 
                       </script>
                       <!-- clock widget end -->
-
                     </div>
                   </div>
                 </div>
