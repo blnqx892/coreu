2d24d716df47f146bb60cc39ae574d2a0c8d83fe
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-09 09:06:23 -0600

    wiget

--- dist/index.php
+++ dist/index.php
@@ -29,7 +29,7 @@
     <div class="body flex-grow-1 px-4">
       <div class="container-lg">
         <!-- WIGET-->
-          <div class="tab-content rounded-bottom; center-block>
+          <div class="tab-content rounded-bottom; center-block">
             <div class="tab-pane p-3 active preview" role="tabpanel" id="preview-1179">
               <div class="row">
                 <div class="col-6 col-lg-3">
