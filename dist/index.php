6b25c118e1d3c126f66d5c89344db2e6ad4fb6bc
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-09 09:09:46 -0600

    wiget

--- dist/index.php
+++ dist/index.php
@@ -29,9 +29,9 @@
     <div class="body flex-grow-1 px-4">
       <div class="container-lg">
         <!-- WIGET-->
-        <div class="center-block">
           <div class="tab-content rounded-bottom">
             <div class="tab-pane p-3 active preview" role="tabpanel" id="preview-1179">
+              <center>
               <div class="row">
                 <div class="col-6 col-lg-3">
                   <div class="card overflow-hidden">
@@ -65,8 +65,8 @@
                   </div>
                 </div>
               </div>
+</center>
             </div>
-          </div>
           </div>
         <!-- /WIGET-->
         <!-- row-->
