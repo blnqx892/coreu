55d93f074a24686b653f9dd96e1f155d2ff77dc0
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-09 10:21:51 -0600

    cvbc

--- dist/ADespachar.php
+++ dist/ADespachar.php
@@ -51,41 +51,7 @@
                     </thead>
                     <tbody>
                       <tr class="align-middle">
-                        <td class="text-center">
-                          <div class="avatar avatar-md"><img class="avatar-img" src="assets/img/avatars/1.jpg"
-                              alt="user@email.com"><span class="avatar-status bg-success"></span></div>
-                        </td>
-                        <td>
-                          <div>Yiorgos Avraamu</div>
-                          <div class="small text-medium-emphasis"><span>New</span> | Registered: Jan 1, 2020</div>
-                        </td>
-                        <td class="text-center">
-                          <svg class="icon icon-xl">
-                            <use xlink:href="vendors/@coreui/icons/svg/flag.svg#cif-us"></use>
-                          </svg>
-                        </td>
-                        <td>
-                          <div class="clearfix">
-                            <div class="float-start">
-                              <div class="fw-semibold">50%</div>
-                            </div>
-                            <div class="float-end"><small class="text-medium-emphasis">Jun 11, 2020 - Jul 10,
-                                2020</small></div>
-                          </div>
-                          <div class="progress progress-thin">
-                            <div class="progress-bar bg-success" role="progressbar" style="width: 50%"
-                              aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                          </div>
-                        </td>
-                        <td class="text-center">
-                          <svg class="icon icon-xl">
-                            <use xlink:href="vendors/@coreui/icons/svg/brand.svg#cib-cc-mastercard"></use>
-                          </svg>
-                        </td>
-                        <td>
-                          <div class="small text-medium-emphasis">Last login</div>
-                          <div class="fw-semibold">10 sec ago</div>
-                        </td>
+                        
                         <td>
                           <div class="dropdown">
                             <button class="btn btn-transparent p-0" type="button" data-coreui-toggle="dropdown"
