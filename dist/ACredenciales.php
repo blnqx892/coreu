6dc02eb5304f8a6a7ec3cc23d18336a08ee7a0cc
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-09 15:31:36 -0600

    utyu

--- dist/ACredenciales.php
+++ dist/ACredenciales.php
@@ -50,28 +50,28 @@
                       <label for="inputZip" class="form-label">No. de Empleados:</label>
                       <input type="number" class="form-control" id="inputZip">
                     </div>
-                  <!--FIN SECCION TRES-->
-                  <div class="row  my-4">
-                    <!--INICIO SECCION CUATRO-->
-                    <div class="col-md-3">
-                      <label for="inputCity" class="form-label">Usuario:</label>
-                      <input type="number" class="form-control" id="inputCity">
+                    <!--FIN SECCION TRES-->
+                    <div class="row  my-4">
+                      <!--INICIO SECCION CUATRO-->
+                      <div class="col-md-3">
+                        <label for="inputCity" class="form-label">Usuario:</label>
+                        <input type="number" class="form-control" id="inputCity">
+                      </div>
+                      <div class="col-md-3">
+                        <label for="inputZip" class="form-label">Contraseña:</label>
+                        <input type="password" class="form-control" id="inputZip">
+                      </div>
+                      <div class="col-md-3">
+                        <label for="inputZip" class="form-label">Repetir Contraseña:</label>
+                        <input type="password" class="form-control" id="inputZip">
+                      </div>
                     </div>
-                    <div class="col-md-3">
-                      <label for="inputZip" class="form-label">Contraseña:</label>
-                      <input type="password" class="form-control" id="inputZip">
+                    <!--FIN SECCION CUATRO-->
+                    <div class="col-15" align="right">
+                      <hr style="color: black; background-color: black; width:100%;" />
+                      <button class="btn btn-primary" type="submit">Guardar</button>
+                      <button class="btn btn-danger" type="submit">Cancelar</button>
                     </div>
-                    <div class="col-md-3">
-                      <label for="inputZip" class="form-label">Repetir Contraseña:</label>
-                      <input type="password" class="form-control" id="inputZip">
-                    </div>
-                  </div>
-                  <!--FIN SECCION CUATRO-->
-                  <div class="col-15" align="right">
-                  <hr style="color: black; background-color: black; width:100%;" /> 
-                    <button class="btn btn-primary" type="submit">Guardar</button>
-                    <button class="btn btn-danger" type="submit">Cancelar</button>
-                  </div>
                 </form>
                 <!--/// FIN FORM ////////////////-->
               </div>
@@ -88,53 +88,37 @@
             <div class="card mb-4">
               <div class="card-header"><strong>Ingreso de Datos</strong></div>
               <div class="card-body">
-              <div class="row  my-4">
-                                        <div>
-                                            <table class="table responsive">
-                                                <thead class="table-dark">
-                                                    <tr>
-                                                        <th>Numero</th>
-                                                        <th>Codigo</th>
-                                                        <th>Fecha</th>
-                                                        <th>Nombre</th>
-                                                        <th>Descripción</th>
-                                                        <th>Modelo</th>
-                                                        <th>Marca</th>
-                                                        <th>Tipo Movimiento</th>
-                                                        <th>Acción</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <th>1</th>
-                                                        <th>0003</th>
-                                                        <th>08/03/2023</th>
-                                                        <th>Silla</th>
-                                                        <th>color negro</th>
-                                                        <th>sh02</th>
-                                                        <th>xs</th>
-                                                        <th>Prestamo</th>
-                                                        <th>
-                                                            <button type="button"
-                                                                class="btn btn-danger rounded-pill"><svg
-                                                                    class="icon me-2">
-                                                                    <use
-                                                                        xlink:href="vendors/@coreui/icons/svg/free.svg#cil-trash">
-                                                                    </use>
-                                                                </svg> Eliminar</button>
-                                                                <button type="button"
-                                                                class="btn btn-danger rounded-pill"><svg
-                                                                    class="icon me-2">
-                                                                    <use
-                                                                        xlink:href="vendors/@coreui/icons/svg/free.svg#fi fi-rr-eye">
-                                                                    </use>
-                                                                </svg> Ver</button>
-                                                        </th>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
+                <div class="row  my-4">
+                  <div>
+                    <table class="table">
+                      <thead class="table-dark">
+                        <tr>
+                          <th>Jefe Unidad</th>
+                          <th>Unidad</th>
+                          <th>Usuario</th>
+                          <th>Acción</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th>1</th>
+                          <th>0003</th>
+                          <th>08/03/2023</th>
+                          <th>
+                            <button type="button" class="btn btn-danger rounded-pill"><svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-trash">
+                                </use>
+                              </svg> Eliminar</button>
+                            <button type="button" class="btn btn-danger rounded-pill"><svg class="icon me-2">
+                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#fi fi-rr-eye">
+                                </use>
+                              </svg> Ver</button>
+                          </th>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
