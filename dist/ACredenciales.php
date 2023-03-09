06d0c2541586b224c1922ad1bbf0e6c000f14c09
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-09 15:19:42 -0600

    vbvcb

--- dist/ACredenciales.php
+++ dist/ACredenciales.php
@@ -79,6 +79,70 @@
           </div>
           <!-- /.row-->
         </div>
+        <div class="row">
+                    <div class="col-12">
+                        <div class="card mb-4">
+                            <div class="card-header"><strong>Actualización Movimientos</strong></div>
+                            <div class="card-body">
+                                <!--INICIO FORM-->
+                                <form class="g-3 needs-validation" novalidate="">
+                                    <div class="row  my-4">
+                                        <div>
+                                            <table class="table">
+                                                <thead class="table-dark">
+                                                    <tr>
+                                                        <th>Numero</th>
+                                                        <th>Codigo</th>
+                                                        <th>Fecha</th>
+                                                        <th>Nombre</th>
+                                                        <th>Descripción</th>
+                                                        <th>Modelo</th>
+                                                        <th>Marca</th>
+                                                        <th>Tipo Movimiento</th>
+                                                        <th>Acción</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <th>1</th>
+                                                        <th>0003</th>
+                                                        <th>08/03/2023</th>
+                                                        <th>Silla</th>
+                                                        <th>color negro</th>
+                                                        <th>sh02</th>
+                                                        <th>xs</th>
+                                                        <th>Prestamo</th>
+                                                        <th>
+                                                            <button type="button"
+                                                                class="btn btn-danger rounded-pill"><svg
+                                                                    class="icon me-2">
+                                                                    <use
+                                                                        xlink:href="vendors/@coreui/icons/svg/free.svg#cil-trash">
+                                                                    </use>
+                                                                </svg> Eliminar</button>
+                                                                <button type="button"
+                                                                class="btn btn-danger rounded-pill"><svg
+                                                                    class="icon me-2">
+                                                                    <use
+                                                                        xlink:href="vendors/@coreui/icons/svg/free.svg#fi fi-rr-eye">
+                                                                    </use>
+                                                                </svg> Ver</button>
+                                                        </th>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                    <!--FIN SECCION TRES-->
+                                    <!--FIN SECCION CUATRO-->
+                               
+                                </form>
+                                <!--/// FIN FORM ////////////////-->
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row-->
+                </div>
       </div>
       <!-- ///////FIN CONTENEDOR/////////////-->
 
