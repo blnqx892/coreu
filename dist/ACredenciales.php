512b1e537dc4e579f1b1e861345a79757060424b
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-13 15:21:05 -0600

    fdgdfg

--- dist/ACredenciales.php
+++ dist/ACredenciales.php
@@ -18,7 +18,16 @@
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb my-0 ms-2">
             <li class="breadcrumb-item">
-              <!-- if breadcrumb is single--><span>Credenciales</span>
+              <a href="index.php"><svg class="icon me-2">
+                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-home">
+                  </use>
+                </svg></a>
+            </li>
+            <li class="breadcrumb-item">
+              <span>Unidad y Restricción</span>
+            </li>
+            <li class="breadcrumb-item active">
+              <span>Credenciales</span>
             </li>
           </ol>
         </nav>
@@ -35,7 +44,8 @@
               <div class="card-body">
                 <div class="col-md-14" align="right">
                   <label for="inputCity" class="form-label">Nueva Credencial</label>
-                  <button type="button" class="btn btn-primary" data-coreui-toggle="modal" data-coreui-target="#exampleModal" data-coreui-whatever="@mdo">Ir</button>
+                  <button type="button" class="btn btn-primary" data-coreui-toggle="modal"
+                    data-coreui-target="#exampleModal" data-coreui-whatever="@mdo">Ir</button>
                 </div>
                 <div class="row  my-4">
                   <table class="table">
@@ -76,68 +86,68 @@
     <!-- ///////FIN CONTENEDOR/////////////-->
     <!-- Scrollable modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
-        <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <div class="card-body">
-                <!--INICIO FORM-->
-                <form class="g-3 needs-validation" novalidate="">
-                  <!--FIN SECCION DOS-->
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+            <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="card-body">
+              <!--INICIO FORM-->
+              <form class="g-3 needs-validation" novalidate="">
+                <!--FIN SECCION DOS-->
+                <div class="row  my-4">
+                  <!--INICIO SECCION TRES-->
+                  <div class="col-md-4">
+                    <label for="inputAddress2" class="form-label">Nombre de Unidad:</label>
+                    <input type="text" class="form-control" id="inputAddress2" placeholder="">
+                  </div>
+                  <div class="col-md-4">
+                    <label for="inputZip" class="form-label">Jefe de Unidad:</label>
+                    <input type="text" class="form-control" id="inputZip">
+                  </div>
+                  <div class="col-md-2">
+                    <label for="inputZip" class="form-label">No. de Empleados:</label>
+                    <input type="number" class="form-control" id="inputZip">
+                  </div>
+                  <!--FIN SECCION TRES-->
                   <div class="row  my-4">
-                    <!--INICIO SECCION TRES-->
-                    <div class="col-md-4">
-                      <label for="inputAddress2" class="form-label">Nombre de Unidad:</label>
-                      <input type="text" class="form-control" id="inputAddress2" placeholder="">
+                    <!--INICIO SECCION CUATRO-->
+                    <div class="col-md-3">
+                      <label for="inputCity" class="form-label">Usuario:</label>
+                      <input type="number" class="form-control" id="inputCity">
                     </div>
-                    <div class="col-md-4">
-                      <label for="inputZip" class="form-label">Jefe de Unidad:</label>
-                      <input type="text" class="form-control" id="inputZip">
+                    <div class="col-md-3">
+                      <label for="inputZip" class="form-label">Contraseña:</label>
+                      <input type="password" class="form-control" id="inputZip">
                     </div>
-                    <div class="col-md-2">
-                      <label for="inputZip" class="form-label">No. de Empleados:</label>
-                      <input type="number" class="form-control" id="inputZip">
+                    <div class="col-md-3">
+                      <label for="inputZip" class="form-label">Repetir Contraseña:</label>
+                      <input type="password" class="form-control" id="inputZip">
                     </div>
-                    <!--FIN SECCION TRES-->
-                    <div class="row  my-4">
-                      <!--INICIO SECCION CUATRO-->
-                      <div class="col-md-3">
-                        <label for="inputCity" class="form-label">Usuario:</label>
-                        <input type="number" class="form-control" id="inputCity">
-                      </div>
-                      <div class="col-md-3">
-                        <label for="inputZip" class="form-label">Contraseña:</label>
-                        <input type="password" class="form-control" id="inputZip">
-                      </div>
-                      <div class="col-md-3">
-                        <label for="inputZip" class="form-label">Repetir Contraseña:</label>
-                        <input type="password" class="form-control" id="inputZip">
-                      </div>
-                    </div>
-                    <!--FIN SECCION CUATRO-->
-                    <div class="col-15" align="right">
-                      <hr style="color: black; background-color: black; width:100%;" />
-                      <button class="btn btn-primary" type="submit">Guardar</button>
-                      <button class="btn btn-danger" type="submit">Cancelar</button>
-                    </div>
-                </form>
-                <!--/// FIN FORM ////////////////-->
-              </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-coreui-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Send message</button>
+                  </div>
+                  <!--FIN SECCION CUATRO-->
+                  <div class="col-15" align="right">
+                    <hr style="color: black; background-color: black; width:100%;" />
+                    <button class="btn btn-primary" type="submit">Guardar</button>
+                    <button class="btn btn-danger" type="submit">Cancelar</button>
+                  </div>
+              </form>
+              <!--/// FIN FORM ////////////////-->
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-coreui-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Send message</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
-    <!-- IMPORTAR ARCHIVO FOOTER-->
-    <?php include("foot/foot.php"); ?>
-    <!-- ////////////////////////-->
-  </div>
+  <!-- IMPORTAR ARCHIVO FOOTER-->
+  <?php include("foot/foot.php"); ?>
+  <!-- ////////////////////////-->
   <!-- IMPORTAR ARCHIVO SCRIPT-->
   <?php include("foot/script.php"); ?>
   <!-- ////////////////////////-->
