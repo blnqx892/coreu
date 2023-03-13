06d9b0b7d7d3fa8b0f6b4ed9864e77843f96335d
Natalie <46360156+natalie72@users.noreply.github.com>
2023-03-13 16:53:17 -0600

    jgj

--- dist/menu/verti.php
+++ dist/menu/verti.php
@@ -181,7 +181,7 @@
           <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-notes"></use>
         </svg> Bitacora</a>
       <ul class="nav-group-items">
-        <li class="nav-item"><a class="nav-link" href=""> Pagina 1</a></li>
+        <li class="nav-item"><a class="nav-link" href="bitacora.php"> Bitacora</a></li>
       </ul>
     </li>
     <li class="nav-group"><a class="nav-link nav-group-toggle" href="#">
