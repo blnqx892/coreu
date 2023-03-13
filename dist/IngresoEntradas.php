4063dcfd7d2917caf197f9e34729b1a0790a755e
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-13 09:16:33 -0600

    evento

--- dist/IngresoEntradas.php
+++ dist/IngresoEntradas.php
@@ -150,10 +150,10 @@
       <script>
         function mostrarFormulario() {
           var formulario = document.getElementById("formulario");
-          if (formulario.style.display === "block") {
-            formulario.style.display = "none";
-          } else {
+          if (formulario.style.display === "none") {
             formulario.style.display = "block";
+          } else {
+            formulario.style.display = "none";
           }
         }
 
