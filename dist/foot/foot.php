3a1b090855935caee6435a9c35cff34650c6ad0a
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-13 11:16:15 -0600

    ghfgh

--- dist/foot/foot.php
+++ dist/foot/foot.php
@@ -1,4 +1,4 @@
 <footer class="footer">
   <div> S I C A F I </div>
-  <div class="ms-auto">UNIVERSIDAD DE EL SALVADOR © 2022</div>
+  <div class="ms-auto">UNIVERSIDAD DE EL SALVADOR © <?php echo date('Y'); ?></div>
 </footer>
