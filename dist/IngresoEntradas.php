5b43242f763c2ce48d756179f37a338b82deb445
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-13 09:54:27 -0600

    fgfd

--- dist/IngresoEntradas.php
+++ dist/IngresoEntradas.php
@@ -103,16 +103,17 @@
                         <label for="inputZip" class="form-label">Descripción:</label>
                         <textarea class="form-control" id="validationCustom02" required="" row="3">
                       </textarea></div>
-                    </div>
-                    <div class="row  my-4">
-                    <div class="col-md-2">
+                      <div class="col-md-2">
                         <label class="form-check-label" for="flexSwitchCheckChecked">Transporte</label>
                         <div class="form-check form-switch">
                           <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked"
                             checked>
                         </div>
                       </div>
-                    <hr style="color: black; background-color: black; width:100%;" /> 
+                    </div>
+                    <div class="row  my-4">
+                      
+                      <hr style="color: black; background-color: black; width:100%;" />
                       <div class="col-md-2">
                         <label for="inputZip" class="form-label">No. Motor:</label>
                         <input type="text" class="form-control" id="inputZip">
@@ -132,7 +133,7 @@
                     </div>
                     <!--FIN SECCION CUATRO-->
                     <div class="col-15" align="right">
-                    <hr style="color: black; background-color: black; width:100%;" /> 
+                      <hr style="color: black; background-color: black; width:100%;" />
                       <button class="btn btn-primary" type="submit">Guardar</button>
                       <button class="btn btn-danger" type="submit">Cancelar</button>
                     </div>
