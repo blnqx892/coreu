f236e571cfd83e00aeaec04c15a9c2b4018d2f28
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-13 11:59:09 -0600

    vbnbv

--- dist/Mdescargo.php
+++ dist/Mdescargo.php
@@ -131,14 +131,12 @@
                 </div>
             </div>
             <!-- ///////FIN CONTENEDOR/////////////-->
-
-            <!-- IMPORTAR ARCHIVO FOOTER-->
-            <?php include("foot/foot.php"); ?>
-            <!-- ////////////////////////-->
         </div>
-        <!-- IMPORTAR ARCHIVO SCRIPT-->
-        <?php include("foot/script.php"); ?>
-        <!-- ////////////////////////-->
+        <!-- IMPORTAR ARCHIVO FOOTER-->
+    <?php include("foot/foot.php"); ?>
+    <!-- IMPORTAR ARCHIVO SCRIPT-->
+    <?php include("foot/script.php"); ?>
+    <!-- ////////////////////////-->
 </body>
 
 </html>
