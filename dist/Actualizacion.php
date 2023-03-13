34fc3a988414c137dcf47785fbcfdfaacb049575
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-13 14:43:32 -0600

    dfgg

--- dist/Actualizacion.php
+++ dist/Actualizacion.php
@@ -17,8 +17,17 @@
       <div class="container-fluid">
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb my-0 ms-2">
+          <li class="breadcrumb-item">
+              <a href="index.php"><svg class="icon me-2">
+                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-home">
+                  </use>
+                </svg></a>
+            </li>
             <li class="breadcrumb-item">
-              <!-- if breadcrumb is single--><span>Adquisiciones</span>
+              <span>Codificcacion de Activos</span>
+            </li>
+            <li class="breadcrumb-item item">
+              <span>Adquisiciones</span>
             </li>
           </ol>
         </nav>
