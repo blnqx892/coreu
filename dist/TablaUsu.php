70ef597a862c80f4b3abc071657ce6d4604e5043
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-14 09:44:22 -0600

    fbnfn

--- dist/TablaUsu.php
+++ dist/TablaUsu.php
@@ -5,97 +5,99 @@
 <!-- ////////////////////////-->
 
 <body>
-    <!-- IMPORTAR ARCHIVO MENU VERTICAL-->
-    <?php include("menu/verti.php"); ?>
-    <!-- ////////////////////////-->
-    <div class="wrapper d-flex flex-column min-vh-100 bg-light">
-        <header class="header header-sticky mb-4">
-            <!-- IMPORTAR ARCHIVO MENU HORIZONTAL-->
-            <?php include("menu/hori.php");?>
-            <!-- ////////////////////////-->
-            <div class="header-divider"></div>
-            <div class="container-fluid">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb my-0 ms-2">
-                        <li class="breadcrumb-item">
-                            <!-- if breadcrumb is single--><span>Seguridad</span>
-                        </li>
-                    </ol>
-                </nav>
-            </div>
-        </header>
-        <!-- CONTENEDOR-->
-        <div class="body flex-grow-1 px-3">
-            <div class="container-lg">
-                <!-- row-->
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card mb-4">
-                            <div class="card-header"><strong>Actualizacion Usuarios</strong></div>
-                            <div class="card-body">
-                                <!--INICIO FORM-->
-                                <form class="g-3 needs-validation" novalidate="">
-                                    <div class="row  my-4">
-                                        <div>
-                                            <table class="table">
-                                                <thead class="table-dark">
-                                                    <tr>
-                                                        <th>Numero</th>
-                                                        <th>Nombre</th>
-                                                        <th>Apellido</th>
-                                                        <th>Email</th>
-                                                        <th>Usuario</th>
-                                                        <th>Acción</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <th>1</th>
-                                                        <th>Blanca</th>
-                                                        <th>Melara</th>
-                                                        <th>BlancaM19@gmail.com</th>
-                                                        <th>Melara01</th>
-                                                         <th>
-                                                            <button type="button"
-                                                                class="btn btn-danger rounded-pill"><svg
-                                                                    class="icon me-2">
-                                                                    <use
-                                                                        xlink:href="vendors/@coreui/icons/svg/free.svg#cil-trash">
-                                                                    </use>
-                                                                </svg> Eliminar</button>
-                                                                <button type="button"
-                                                                class="btn btn-danger rounded-pill"><svg
-                                                                    class="icon me-2">
-                                                                    <use
-                                                                        xlink:href="vendors/@coreui/icons/svg/free.svg#fi fi-rr-eye">
-                                                                    </use>
-                                                                </svg> Ver</button>
-                                                        </th>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                    <!--FIN SECCION TRES-->
-                                    <!--FIN SECCION CUATRO-->
-                               
-                                </form>
-                                <!--/// FIN FORM ////////////////-->
-                            </div>
-                        </div>
+  <!-- IMPORTAR ARCHIVO MENU VERTICAL-->
+  <?php include("menu/verti.php"); ?>
+  <!-- ////////////////////////-->
+  <div class="wrapper d-flex flex-column min-vh-100 bg-light">
+    <header class="header header-sticky mb-4">
+      <!-- IMPORTAR ARCHIVO MENU HORIZONTAL-->
+      <?php include("menu/hori.php");?>
+      <!-- ////////////////////////-->
+      <div class="header-divider"></div>
+      <div class="container-fluid">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb my-0 ms-2">
+            <li class="breadcrumb-item">
+              <a href="index.php"><svg class="icon me-2">
+                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-home">
+                  </use>
+                </svg></a>
+            </li>
+            <li class="breadcrumb-item">
+              <span>Control de Usuarios</span>
+            </li>
+            <li class="breadcrumb-item active">
+              <span>Actualización</span>
+            </li>
+          </ol>
+        </nav>
+      </div>
+    </header>
+    <!-- CONTENEDOR-->
+    <div class="body flex-grow-1 px-3">
+      <div class="container-lg">
+        <!-- row-->
+        <div class="row">
+          <div class="col-12">
+            <div class="card mb-4">
+              <div class="card-header"><strong>Actualizacion Usuarios</strong></div>
+              <div class="card-body">
+                <!--INICIO FORM-->
+                <form class="g-3 needs-validation" novalidate="">
+                  <div class="row  my-4">
+                    <div>
+                      <table class="table">
+                        <thead class="table-dark">
+                          <tr>
+                            <th>Numero</th>
+                            <th>Nombre</th>
+                            <th>Apellido</th>
+                            <th>Email</th>
+                            <th>Usuario</th>
+                            <th>Acción</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th>1</th>
+                            <th>Blanca</th>
+                            <th>Melara</th>
+                            <th>BlancaM19@gmail.com</th>
+                            <th>Melara01</th>
+                            <th>
+                              <button type="button" class="btn btn-danger rounded-pill"><svg class="icon me-2">
+                                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-trash">
+                                  </use>
+                                </svg> Eliminar</button>
+                              <button type="button" class="btn btn-danger rounded-pill"><svg class="icon me-2">
+                                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#fi fi-rr-eye">
+                                  </use>
+                                </svg> Ver</button>
+                            </th>
+                          </tr>
+                        </tbody>
+                      </table>
                     </div>
-                    <!-- /.row-->
-                </div>
-            </div>
-            <!-- ///////FIN CONTENEDOR/////////////-->
+                  </div>
+                  <!--FIN SECCION TRES-->
+                  <!--FIN SECCION CUATRO-->
 
-            <!-- IMPORTAR ARCHIVO FOOTER-->
-            <?php include("foot/foot.php"); ?>
-            <!-- ////////////////////////-->
+                </form>
+                <!--/// FIN FORM ////////////////-->
+              </div>
+            </div>
+          </div>
+          <!-- /.row-->
         </div>
-        <!-- IMPORTAR ARCHIVO SCRIPT-->
-        <?php include("foot/script.php"); ?>
-        <!-- ////////////////////////-->
+        <!-- ///////FIN CONTENEDOR/////////////-->
+      </div>
+      <!-- IMPORTAR ARCHIVO FOOTER-->
+      <?php include("foot/foot.php"); ?>
+      <!-- ////////////////////////-->
+      <!-- IMPORTAR ARCHIVO SCRIPT-->
+      <?php include("foot/script.php"); ?>
+      <!-- ////////////////////////-->
+    </div>
 </body>
 
 </html>
