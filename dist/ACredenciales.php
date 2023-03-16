028a1fcb873cf4c0058051a69df6952315aa39bd
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-16 09:07:15 -0600

    vxv

--- dist/ACredenciales.php
+++ dist/ACredenciales.php
@@ -93,14 +93,16 @@
             <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <div class="card-body">
-              <!--INICIO FORM-->
-              <form class="g-3 needs-validation" novalidate="">
-                <!--FIN SECCION DOS-->
-                
-              </form>
-              <!--/// FIN FORM ////////////////-->
-            </div>
+          <form>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Recipient:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-coreui-dismiss="modal">Close</button>
