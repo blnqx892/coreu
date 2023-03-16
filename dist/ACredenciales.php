6dc7eb91106c45fa411efa4a1a98639110594e64
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-16 09:52:25 -0600

    gfgf

--- dist/ACredenciales.php
+++ dist/ACredenciales.php
@@ -85,7 +85,7 @@
     </div>
     <!-- ///////FIN CONTENEDOR/////////////-->
     <!-- Scrollable modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal" tabindex="" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
