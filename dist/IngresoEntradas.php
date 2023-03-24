becd282ad1443575ba3c69b5fb2836b5efa8668a
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-24 13:09:39 -0600

    fgg

--- dist/IngresoEntradas.php
+++ dist/IngresoEntradas.php
@@ -174,12 +174,12 @@
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Agregar Nueva Categoria</h5>
               <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
               <div class="row  my-4">
-                <div class="col-md-4">
+                <div class="col-md-6">
                   <label for="inputCity" class="form-label">Nombre:</label>
                   <input type="text" class="form-control" id="inputCity">
                 </div>
