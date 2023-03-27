3100803969ca44976b5e59c07f7729637b7ec7ce
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-27 10:54:40 -0600

    fdgdfg

--- dist/CatalogoAdquisicion.php
+++ dist/CatalogoAdquisicion.php
@@ -43,47 +43,47 @@
               <div class="card-header"><strong>Tabla de Adquisiciones</strong></div>
               <div class="card-body">
                 <!-- dataTable-->
-            <table id="example" class="display" style="width:100%">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Position</th>
-                  <th>Office</th>
-                  <th>Age</th>
-                  <th>Start date</th>
-                  <th>Salary</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Tiger Nixon</td>
-                  <td>System Architect</td>
-                  <td>Edinburgh</td>
-                  <td>61</td>
-                  <td>2011-04-25</td>
-                  <td>$320,800</td>
-                </tr>
-                <tr>
-                  <td>Garrett Winters</td>
-                  <td>Accountant</td>
-                  <td>Tokyo</td>
-                  <td>63</td>
-                  <td>2011-07-25</td>
-                  <td>$170,750</td>
-                </tr>
-              </tbody>
-              <tfoot>
-                <tr>
-                  <th>Name</th>
-                  <th>Position</th>
-                  <th>Office</th>
-                  <th>Age</th>
-                  <th>Start date</th>
-                  <th>Salary</th>
-                </tr>
-              </tfoot>
-            </table>
-            <!-- //dataTable-->
+                <table id="example" class="display" style="width:100%">
+                  <thead>
+                    <tr>
+                      <th>Nombre</th>
+                      <th>N° Factura</th>
+                      <th>Marca</th>
+                      <th>Categoria</th>
+                      <th>Fecha</th>
+                      <th>Acción</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Tiger Nixon</td>
+                      <td>System Architect</td>
+                      <td>Edinburgh</td>
+                      <td>61</td>
+                      <td>2011-04-25</td>
+                      <td>$320,800</td>
+                    </tr>
+                    <tr>
+                      <td>Garrett Winters</td>
+                      <td>Accountant</td>
+                      <td>Tokyo</td>
+                      <td>63</td>
+                      <td>2011-07-25</td>
+                      <td>$170,750</td>
+                    </tr>
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <th>Nombre</th>
+                      <th>N° Factura</th>
+                      <th>Marca</th>
+                      <th>Categoria</th>
+                      <th>Fecha</th>
+                      <th>Acción</th>
+                    </tr>
+                  </tfoot>
+                </table>
+                <!-- //dataTable-->
               </div>
             </div>
           </div>
