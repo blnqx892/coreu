a1f3f9d12c91d005f2bdcd85e16dc9cc523a4721
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-27 12:29:19 -0600

    fvvf

--- dist/head/head.php
+++ dist/head/head.php
@@ -48,10 +48,8 @@
 
   </script>
   <link href="vendors/@coreui/chartjs/css/coreui-chartjs.css" rel="stylesheet">
-  <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
-  <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 
-  <script defer src="/your_path_to_version_6_files/js/fontawesome.js"></script>
+  <script src="https://kit.fontawesome.com" crossorigin="anonymous"></script>
 
   <!-- dataTable-->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
