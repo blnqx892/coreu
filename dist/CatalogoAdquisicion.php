5f79ced239e70da34fccfbeb6bbc2aedd70c7b09
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-03-27 11:08:25 -0600

    dsfv

--- dist/CatalogoAdquisicion.php
+++ dist/CatalogoAdquisicion.php
@@ -63,7 +63,7 @@
                       <td>2011-04-25</td>
                       <td><button type="button" class="btn btn-info rounded-pill">Ver <i
                                   class='far fa-eye'></i></button>
-                              <button type="button" class="btn btn-warning rounded-pill">Codificar <i class="fad fa-barcode-read"></i></button></td>
+                              <button type="button" class="btn btn-warning rounded-pill">Codificar <i class='fad fa-barcode-read'></i></button></td>
                     </tr>
                     <tr>
                       <td>Garrett Winters</td>
@@ -73,7 +73,8 @@
                       <td>2011-07-25</td>
                       <td><button type="button" class="btn btn-info rounded-pill">Ver <i
                                   class='far fa-eye'></i></button>
-                              <button type="button" class="btn btn-warning rounded-pill">Codificar <i class="fad fa-barcode-read"></i></button></td>
+                              <button type="button" class="btn btn-warning rounded-pill">Codificar <i
+                                  class='far fa-trash-alt'></i></button></td>
                     </tr>
                   </tbody>
                   <tfoot>
