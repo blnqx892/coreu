a14b4cd94226888ec49842f2ff34eb700c6ef160
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-04-05 09:51:08 -0600

    fghg

--- dist/TablaSumi.php
+++ dist/TablaSumi.php
@@ -61,9 +61,11 @@
                       <td>Lapiz Facela</td>
                       <td>$ 4.75</td>
                       <td>Estante A3</td>
-                      <td><button type="button" class="btn btn-info rounded-pill" title="Ver"><i
-                            class='far fa-eye'></i></button>
+                      <td>
+                            <button type="button" class="btn btn-info rounded-pill" title="Ver"><iclass='far fa-eye'></i></button>
                             <button type="button" class="btn btn-warning rounded-pill" title="Editar"><i class="far fa-edit"></i></button>
+                            <button type="button" class="btn btn-success rounded-pill" title="Alta"><i class="fa-solid fa-arrow-up-long"></i></button>
+                            <button type="button" class="btn btn-danger rounded-pill" title="Baja"><i class="fa-solid fa-arrow-down-long"></i></i></button>
                       </td>
                     </tr>
                     <tr>
@@ -72,8 +74,8 @@
                       <td>Resma papel bond</td>
                       <td>$ 6.25</td>
                       <td>Estante A2</td>
-                      <td><button type="button" class="btn btn-info rounded-pill" title="Ver"><i
-                            class='far fa-eye'></i></button>
+                      <td>
+                            <button type="button" class="btn btn-info rounded-pill" title="Ver"><iclass='far fa-eye'></i></button>
                             <button type="button" class="btn btn-warning rounded-pill" title="Editar"><i class="far fa-edit"></i></button>
                             <button type="button" class="btn btn-success rounded-pill" title="Alta"><i class="fa-solid fa-arrow-up-long"></i></button>
                             <button type="button" class="btn btn-danger rounded-pill" title="Baja"><i class="fa-solid fa-arrow-down-long"></i></i></button>
