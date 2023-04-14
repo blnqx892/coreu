a077bc19e4622cf296a601555b0b371de43e5f57
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-04-14 11:37:36 -0600

    prueba

--- dist/prueba.php
+++ dist/prueba.php
@@ -1,27 +1,32 @@
-
 <!DOCTYPE html>
 <html>
-    <head>       
-        <meta charset="utf-8">
-        <title>Ejemplo del uso de tablas- aprenderaprogramar.com</title>
-    </head>
-    <body>
-        <table border="2px"> <!-- Lo cambiaremos por CSS -->
-            <tr>
-                <th>Alumno</th>
-                <th>Asignatura</th>
-                <th>Calificación</th>
-            </tr>
-            <tr>
-                <td>Juan Pérez</td>
-                <td>Matemáticas</td>            
-                <td>9</td>
-            </tr>
-            <tr>
-                <td>Rodolfo Cárdenas</td>
-                <td>Francés</td>            
-                <td>5</td>
-            </tr>
-        </table>
-    </body>
+
+<head>
+  <meta charset="utf-8">
+  <title>Ejemplo del uso de tablas- aprenderaprogramar.com</title>
+</head>
+
+<body>
+  <TABLE BORDER="1">
+    <TR>
+      <TH COLSPAN="2">
+        Celda de dos columnas
+      </TH>
+    </TR>
+    <TR>
+      <TD ROWSPAN="2">
+        Celda de dos filas
+      </TD>
+      <TD>
+        Fila2,Columna2
+      </TD>
+    </TR>
+    <TR>
+      <TD>
+        Fila3,Columna2
+      </TD>
+    </TR>
+  </TABLE>
+</body>
+
 </html>
