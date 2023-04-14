a7e4e34fde15fe0873475735afd513e6edbb6f67
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-04-13 18:27:07 -0600

    kardex

---
+++ dist/Kardex.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- IMPORTAR ARCHIVO CABECERA-->
+<?php include("head/head.php"); ?>
+<!-- ////////////////////////-->
+
+<body>
+  <!-- IMPORTAR ARCHIVO MENU VERTICAL-->
+  <?php include("menu/verti.php"); ?>
+  <!-- ////////////////////////-->
+  <div class="wrapper d-flex flex-column min-vh-100 bg-light">
+    <header class="header header-sticky mb-4">
+      <!-- IMPORTAR ARCHIVO MENU HORIZONTAL-->
+      <?php include("menu/hori.php");?>
+      <!-- ////////////////////////-->
+      <div class="header-divider"></div>
+      <div class="container-fluid">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb my-0 ms-2">
+          <li class="breadcrumb-item">
+              <a href="index.php"><svg class="icon me-2">
+                  <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-home">
+                  </use>
+                </svg></a>
+          </li>
+            <li class="breadcrumb-item">
+              <span>Kardex</span>
+            </li>
+            <li class="breadcrumb-item active">
+              <span>Kardex</span>
+            </li>
+          </ol>
+        </nav>
+      </div>
+    </header>
+    <!-- CONTENEDOR-->
+    <div class="body flex-grow-1 px-3">
+      <div class="container-lg">
+        <!-- row-->
+        <div class="row">
+          <div class="col-12">
+            <div class="card mb-4">
+              <div class="card-header"><strong>Kardex</strong></div>
+              <div class="card-body">
+                <!-- dataTable-->
+                <table id="example" class="display" style="width:100%" cellpadding="0" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th style="text-align:center;">Fecha</th>
+                      <th style="text-align:center;">Categoria</th>
+                      <th style="text-align:center;">Nombre</th>
+                      <th style="text-align:center;">Marca</th>
+                      <th style="text-align:center;">Ubicación</th>
+                      <th style="text-align:center;">Acción</th>
+                    </tr>
+                  </thead>
+                  <tbody style="text-align:center;">
+                    <tr>
+                      <td>12-11-2022</td>
+                      <td>Equipo de Oficina</td>
+                      <td>Estante Metalico</td>
+                      <td>Dexion</td>
+                      <td>Unidad de Estado Familiar</td>
+                      <td><button type="button" class="btn btn-info rounded-pill" title="Ver"><i
+                            class='far fa-eye'></i></button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>9-1-2023</td>
+                      <td>Equipo Intangible</td>
+                      <td>Software Oficce</td>
+                      <td>Microsoft</td>
+                      <td>Unidad de UACI</td>
+                      <td><button type="button" class="btn btn-info rounded-pill" title="Ver"><i
+                            class='far fa-eye'></i></button>
+                      </td>
+                    </tr>
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <th style="text-align:center;">Fecha</th>
+                      <th style="text-align:center;">Categoria</th>
+                      <th style="text-align:center;">Nombre</th>
+                      <th style="text-align:center;">Marca</th>
+                      <th style="text-align:center;">Ubicación</th>
+                      <th style="text-align:center;">Acción</th>
+                    </tr>
+                  </tfoot>
+                </table>
+                <!-- //dataTable-->
+              </div>
+            </div>
+          </div>
+          <!-- /.row-->
+        </div>
+      </div>
+      <!-- ///////FIN CONTENEDOR/////////////-->
+    </div>
+    <!-- IMPORTAR ARCHIVO FOOTER-->
+    <?php include("foot/foot.php"); ?>
+    <!-- IMPORTAR ARCHIVO SCRIPT-->
+    <?php include("foot/script.php"); ?>
+    <!-- ////////////////////////-->
+  </div>
+</body>
+
+</html>
