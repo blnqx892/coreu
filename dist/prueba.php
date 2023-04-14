4f2d56fe4a1198d5014b38f1145ec0472629493b
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-04-14 11:34:26 -0600

    prueba

--- dist/prueba.php
+++ dist/prueba.php
@@ -6,16 +6,20 @@
     </head>
     <body>
         <table border="2px"> <!-- Lo cambiaremos por CSS -->
-           <tr>
-              <td>Celda 1</td>
-              <td>Celda 2</td>
-              <td>Celda 3</td>
-          </tr>
-          <tr>
-              <td>Celda 4</td>
-              <td>Celda 5</td>
-              <td>Celda 6</td>
-          </tr>
+            <tr>
+                <td>Campo 1</td>
+                <td>Campo 2</td>
+                <td>Campo 3</td>
+            </tr>
+            <tr>
+                <td colspan="2">Campos 4 y 5</td>
+                <td>Campo 6</td>
+            </tr>
+            <tr>
+                <td>Campo 7</td>
+                <td>Campo 8</td>
+                <td>Campo 9</td>
+            </tr>
         </table>
     </body>
 </html>
