0824f59dd6ed26edb02ceab1cbdbb5022db655fb
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-04-14 11:24:47 -0600

    kardex

--- dist/Kardex.php
+++ dist/Kardex.php
@@ -46,10 +46,10 @@
                 <table id="example" class="display" style="width:100%" cellpadding="0" cellspacing="0">
                   <thead>
                     <tr>
-                      <th style="text-align:center;">Fecha</th>
-                      <th style="text-align:center;">Categoria</th>
-                      <th style="text-align:center;">Nombre</th>
-                      <th style="text-align:center;">Marca</th>
+                      <th style="text-align:center;">Codigo</th>
+                      <th style="text-align:center;">Nombre Suministro</th>
+                      <th style="text-align:center;">Tarjeta N°</th>
+                      <th style="text-align:center;">Existencias Max/Min</th>
                       <th style="text-align:center;">Ubicación</th>
                       <th style="text-align:center;">Acción</th>
                     </tr>
@@ -78,10 +78,10 @@
                   </tbody>
                   <tfoot>
                     <tr>
-                      <th style="text-align:center;">Fecha</th>
-                      <th style="text-align:center;">Categoria</th>
-                      <th style="text-align:center;">Nombre</th>
-                      <th style="text-align:center;">Marca</th>
+                      <th style="text-align:center;">Codigo</th>
+                      <th style="text-align:center;">Nombre Suministro</th>
+                      <th style="text-align:center;">Tarjeta N°</th>
+                      <th style="text-align:center;">Existencias Max/Min</th>
                       <th style="text-align:center;">Ubicación</th>
                       <th style="text-align:center;">Acción</th>
                     </tr>
