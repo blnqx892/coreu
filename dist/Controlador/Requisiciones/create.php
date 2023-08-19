9b3c2b4be23725a9b1c5376fec51a37a785d8f97
carlos.ruiz <carlos.ruiz@medicamentos.gob.sv>
2023-08-18 20:09:22 -0600

    :bug: guardar requisicion

--- dist/Controlador/Requisiciones/create.php
+++ dist/Controlador/Requisiciones/create.php
@@ -39,31 +39,25 @@ mysqli_query($conexion, $q_req);
 
 $q_suministro = "";
 // Guardar los suministros
+$is_ok = true;
 foreach ($body->suministros as $suministro) {
   $id2 = intval(microtime(true)*1000000);
 
-  $q_suministro .= "insert into detalle_requisicion (
-                                 id,
-                                 requisicion_id,
-                                 suministro_id,
-                                 cantidad_solicitada
-) values (
-          ".$id2.",
-          ".$timestamp.",
-          ".$suministro->suministroId.",
-          ".$suministro->cantidad."
-);";
+  $q_suministro = "insert into detalle_requisicion (id, requisicion_id, suministro_id, cantidad_solicitada) values (".$id2.",".$timestamp.",".$suministro->suministroId.",".$suministro->cantidad.");";
+  if (!mysqli_query($conexion, $q_suministro)) {
+    $is_ok = false;
+  }
 }
 
-echo json_encode($q_suministro);
-
-$response["statusCode"] = 500;
-$response["message"] = "Algo salió mal, no se pudo guardar";
-
-if (mysqli_query($conexion, $q_suministro)) {
+if ($is_ok) {
   $response["statusCode"] = 200;
   $response["data"] = $timestamp;
   $response["message"] = "Registro guardado";
+} else {
+  $response["statusCode"] = 500;
+  $response["message"] = "Algo salió mal, no se pudo guardar";
 }
+
+
 
 echo json_encode($response);
