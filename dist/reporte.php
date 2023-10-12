557a6e219f29e13f62d8f01e8343929c35a97fb0
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-10-12 16:06:06 -0600

    creando reporte

---
+++ dist/reporte.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Reporte de ventas</title>
+</head>
+<body>
+  <header>
+    <h1>Reporte de ventas</h1>
+    <p>Fecha de creación: 2023-08-02</p>
+    <p>Empresa: Mi empresa</p>
+  </header>
+  <body>
+    <table>
+      <thead>
+        <tr>
+          <th>Producto</th>
+          <th>Cantidad</th>
+          <th>Precio</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Producto 1</td>
+          <td>10</td>
+          <td>$100</td>
+          <td>$1000</td>
+        </tr>
+        <tr>
+          <td>Producto 2</td>
+          <td>20</td>
+          <td>$200</td>
+          <td>$4000</td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+  <footer>
+    <p>Número de páginas: 1</p>
+    <p>Contacto: info@miempresa.com</p>
+  </footer>
+</body>
+</html>
