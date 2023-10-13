2d5a0ddb08e5a6b4b4d1aff5b8b762e5383a1d40
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-10-13 16:12:28 -0600

    reporte3

--- dist/reporte3.php
+++ dist/reporte3.php
@@ -31,7 +31,7 @@
     </tr>
   </table><br>
 
-  <table align="center">
+  <table width="950" align="center">
     <thead>
       <tr>
         <th colspan="4"></th>
@@ -39,89 +39,81 @@
     </thead>
     <tbody style="color:#00000;font-size:125%;">
       <tr>
-        <td style="width: 90px;"><b>FECHA DE SOLICITUD:</b></td>
-        <td style="width: 100px;">13/10/2023</td>
-        <td style="width: 90px;"><b>FECHA DE DESPACHO</b></td>
-        <td style="width: 100px;">19/10/2023</td>
-
+        <td><b>FECHA DE SOLICITUD:</b></td>
+        <td> 13/10/2023 </td>
+        <td><b>FECHA DE DESPACHO: </b></td>
+        <td> <?php echo date("d/m/Y"); ?> </td>
+      </tr>
+      <tr>
+        <td><b>UNIDAD SOLICITADA: </b></td>
+        <td>UNIDAD DE CARNET DE MINORIDAD</td>
       </tr>
     </tbody>
   </table><br>
 
-  <table frame="box" align="center">
+  <table border="1" align="center" width="950">
     <thead>
       <tr>
-        <th colspan="4">VEHICULO</th>
+        <th>NÚMERO</th>
+        <th>CÓDIGO</th>
+        <th>U/MEDIDA</th>
+        <th>DESCRIPCIÓN DE MATERIAL O INSUMO</th>
+        <th>CANTIDAD SOLICITADA</th>
+        <th>CANTIDAD DESPACHADA</th>
+        <th>PRECIO UNITARIO</th>
+        <th>COSTO TOTAL</th>
       </tr>
     </thead>
-
     <tbody style="color:#00000;font-size:135%;">
-      <tr>
-        <td><b>No. Motor:</b></td>
-        <td>num234344</td>
-      </tr>
-      <tr>
-        <td><b>No. Chasis: </b></td>
-        <td>424344</td>
-      </tr>
-      <tr>
-        <td><b>Capacidad: </b></td>
-        <td>56</td>
-        <td><b>Placa: </b></td>
-        <td>p56-977</td>
-      </tr>
-    </tbody>
-  </table><br><br><br><br>
-
-  <table align="center" width="685">
-    <thead>
-      <tr>
-        <th colspan="2">PERSONA QUE RECIBE EL BIEN</th>
-        <th colspan="2">PERSONA QUE ENTREGA EL BIEN</th>
-      </tr>
-    </thead>
-
-    <tbody style="color:#00000;font-size:135%;">
-      <tr>
-        <td><b>Nombre:</b></td>
-        <td>Harry Potter</td>
-        <td><b>Nombre:</b></td>
-        <td>Ronald Wesley</td>
-
-      </tr>
-      <tr>
-        <td><b>Cargo: </b></td>
-        <td>Jefe Unidad RRHH</td>
-        <td><b>Cargo: </b></td>
-        <td>Jefe Activo Fijo</td>
-      </tr>
-      <tr>
-        <td><b>Firma: </b></td>
-        <td>__________________</td>
-        <td><b>Firma: </b></td>
-        <td>__________________</td>
-      </tr>
-      <tr>
-        <td><b></b></td>
-        <td></td>
-        <td><b></b></td>
-        <td>Jefe de Activo Fijo</td>
-      </tr>
-    </tbody>
-  </table><br><br>
-  <table align="right">
     <tr>
-      <td>Fecha de cargo: <?php echo date("d/m/Y"); ?>
-        <br>
-        Hora: <?php
-		date_default_timezone_set('America/El_Salvador');
-		$date = new DateTime();
-	     echo $date->format('h:i:s A');
-		?></td>
+      <td>1</td>
+      <td>0989</td>
+      <td>Caja</td>
+      <td>Lapiceros Facela Color Azul</td>
+      <td>2</td>
+      <td>2</td>
+      <td>$ 6.00</td>
+      <td>$ 12.00</td>
     </tr>
+    </tbody>
   </table>
+  <table frame="box" align="center" width="950">
+    <thead>
+      <tr>
+        <td colspan="4">OBSERVACIONES: </td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td colspan="4">Ahi van unas observaciones </td>
+      </tr>
+    </tbody>
+  </table><br>
 
-
+  <table  align="center" width="950">
+    <thead>
+      <tr>
+        <td>F:  </td>
+        <td>F:  </td>
+        <td>F:  </td>
+        <td>F:  </td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td >Pancracio Joaquin Barraza Perez </td>
+        <td >Antonieta de las Nieves</td>
+        <td >Wisin y Yandel los Extraterrestres </td>
+        <td >El Chapulin Colorado</td>
+      </tr>
+      <tr>
+        <td >NOMBRE QUIEN SOLICITA </td>
+        <td >NOMBRE QUIEN AUTORIZA</td>
+        <td >NOMBRE QUIEN RECIBE </td>
+        <td >NOMBRE QUIEN DESPACHA</td>
+      </tr>
+    </tbody>
+  </table>
   <form name="frmTesis" method="get" action="" id="frmTesis">
     <p align="center"><input class="btn btn-primary" data-toggle="modal" data-target="#modalNuevo"
         style="font-size:17px;" type="button" name="IM" id="IM" value="IMPRIMIR" onClick="imprimir()"></p>
