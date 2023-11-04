19f94b332078f8ff80196813782c34fca464a352
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-11-04 11:43:12 -0600

    detalles del acceso

--- dist/Acceso.php
+++ dist/Acceso.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="css/fondo.css" rel="stylesheet">
@@ -17,14 +18,16 @@
         <fieldset class="len">
           <legend class="len2">Acceder</legend><br>
           <label for="uname">Usuario </label><br>
-          <input class="input-field" type="text" id="username" name="username" required size="10" style="font-family: Arial; font-size: 16pt;"><br>
-
+          <input class="input-field" type="text" id="username" name="username" required size="10"
+            style="font-family: Arial; font-size: 16pt; width : 380px; heigth : 1px"><br>
+          <br>
           <label for="psw">Contraseña</label><br>
-          <input class="input-field" type="password" id="password" name="password" required size="10" style="font-family: Arial; font-size: 16pt;"><br><br>
+          <input class="input-field" type="password" id="password" name="password" required size="10"
+            style="font-family: Arial; font-size: 16pt;width : 380px; heigth : 1px"><br><br>
         </fieldset><br><br>
         <center>
-          <button type="submit" class="btns btn-info">Iniciar Sesión</button>
-          <button type="submit" class="btns btn-danger">Cancelar</button><br><br><br>
+          <button type="submit" class="btns btn-info" style="color: white;">Iniciar Sesión</button>
+          <button type="submit" class="btns btn-danger" style="color: white;">Cancelar</button><br><br><br>
           <a href="olvido_contrasena.php">¿Has olvidado tu contraseña?</a>
         </center>
       </form>
