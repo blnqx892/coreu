fea97a1f6f3cf6704784ae9c31fb17d5e0539ee0
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-11-21 16:48:34 -0600

    reporte kardex actaulizando

--- dist/Recuperar/guarda_pass.php
+++ dist/Recuperar/guarda_pass.php
@@ -2,6 +2,10 @@
 	require '../Confi/confu.php';
   include '../Confi/funcs.php';
 
+  echo '<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>';
+  echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>';
+  echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>';
+
 	$user_id = $mysqli->real_escape_string($_POST['user_id']);
 	$token = $mysqli->real_escape_string($_POST['token']);
 	$password = $mysqli->real_escape_string($_POST['password']);
@@ -14,18 +18,60 @@
 
 		if(cambiaPassword($pass_hash, $user_id, $token))
 		{
-			echo "Contraseña Modificada <br> <a href='../Acceso.php' >Iniciar Sesion</a>";
+      echo
+      "<script language='javascript'>
+          $(document).ready(function () {
+              setTimeout(function () {
+                  Swal.fire({
+                      title: 'Contraseña modificada',
+                      text: 'Se a reestablecido tu contraseña',
+                      icon: 'success',
+                      confirmButtonText: 'Aceptar'
+                  }).then((result) => {
+                      if (result.value) {
+                          window.location='../Acceso.php';
+                      }
+                  })
+              }, 1000);
+          });
+      </script>";
 			} else {
-
-			echo "Error al modificar contraseña";
-
+        echo
+        "<script language='javascript'>
+            $(document).ready(function () {
+                setTimeout(function () {
+                    Swal.fire({
+                        title: 'Error',
+                        text: 'Algo salio mal, intentelo nuevamente.',
+                        icon: 'Error',
+                        confirmButtonText: 'Aceptar'
+                    }).then((result) => {
+                        if (result.value) {
+                            window.location='../Acceso.php';
+                        }
+                    })
+                }, 1000);
+            });
+        </script>";
 		}
 
 		} else {
-
-		echo 'Las contraseñas no coinciden';
-
+      echo
+      "<script language='javascript'>
+          $(document).ready(function () {
+              setTimeout(function () {
+                  Swal.fire({
+                      title: 'Advertencia',
+                      text: 'Las contraseñas no coinciden',
+                      icon: 'warning',
+                      confirmButtonText: 'Aceptar'
+                  }).then((result) => {
+                      if (result.value) {
+                          window.location='../Acceso.php';
+                      }
+                  })
+              }, 1000);
+          });
+      </script>";
 	}
-
-
 ?>
--- dist/Reportes/Kardex.php
+++ dist/Reportes/Kardex.php
@@ -7,72 +7,99 @@
 </head>
 
 <body style="margin: 30px 30px 20px 20px;">
-  <table width="1000" align="center" border="1" style="color:#00000;font-size:120%;">
+  <table width="1000" align="center" border="1" class="table_informacion">
     <tr>
-      <td width=90>Codigo: </td>
-      <td rowspan="4" width=70 align="center">ALCALDÍA MUNICIPAL DE SAN VICENTE <br><br>
-        CATALOGO DE SUMINISTROS EN EXISTENCIAS</td>
-      <td width=100>Tarjeta No.:</td>
+      <td width=90>Codigo: <b>12345678</b></td>
+      <td rowspan="4" width=70 align="center"><br><br><b>ALCALDÍA MUNICIPAL DE SAN VICENTE <br><br>
+        CONTROL DE EXISTENCIA <br>DE SUMINISTROS</b><br><br></td>
+
+      <td width=100>Tarjeta No.: <b>12</b></td>
     </tr>
     <tr>
-      <td>Nombre del Articulo: </td>
-      <td>Almacen: </td>
+      <td>Nombre del Articulo: <b>Lapiceros Bic</b></td>
+      <td>Almacen: <b>3</b></td>
     </tr>
     <tr>
-      <td>Presentación: </td>
-      <td>Existencias: </td>
+      <td>Presentación: <b>Caja</b></td>
+      <td>Existencias: <b>22</b></td>
     </tr>
     <tr>
-      <td>Unidad de Medida:</td>
-      <td>Max: Min: </td>
+      <td>Unidad de Medida: <b>Caja</b></td>
+      <td colspan="2">Max: <b>50</b> ||    Min: <b>10</b></td>
     </tr>
     <tr>
       <td colspan="">Estante: </td>
       <td colspan="">Entrepaño: </td>
       <td colspan="">Casilla: </td>
     </tr>
-  </table>
+  </table><br>
   <div class="">
     <table border="1" class="table_informacion">
       <thead>
         <tr>
-          <th>FECHA</th>
-          <th>CONCEPTO</th>
-          <th>FONDO PROCEDENCIA</th>
-          <th>ENTRADAS</th>
-          <th>SALIDAS</th>
-          <th>SALDO DE ARTICULOS</th>
+          <th rowspan="2">FECHA</th>
+          <th rowspan="2">CONCEPTO</th>
+          <th rowspan="2">FONDOS PROCEDENCIA</th>
+          <th colspan="2">ENTRADAS</th>
+          <th colspan="2">SALIDAS</th>
+          <th rowspan="2">SALDOS ARTICULOS</th>
+        </tr>
+        <tr>
+          <th>CANTIDAD </th>
+          <th>PRECIO</th>
+          <th>CANTIDAD </th>
+          <th>PRECIO</th>
         </tr>
       </thead>
-      <tbody style="color:#00000;font-size:100%;">
+      <tbody style="color:#00000;font-size:100%;" align="center">
         <tr>
+          <td>12/11/2023</td>
+          <td>Compra de Lapiceros Bic</td>
           <td>1</td>
-          <td>Escritorio negro</td>
-          <td>Imperial</td>
-          <td>imp1234</td>
-          <td>Luxur</td>
-          <td>12/08/2022</td>
+          <td>30</td>
+          <td>2.50</td>
+          <td>0</td>
+          <td>0</td>
+          <td>30</td>
+        </tr>
+        <tr>
+        <td>15/11/2023</td>
+          <td>Salida de requisición: 1699303736</td>
+          <td>1</td>
+          <td>0</td>
+          <td>0</td>
+          <td>5</td>
+          <td>2.50</td>
+          <td>25</td>
+        </tr>
+        <td>16/11/2023</td>
+          <td>Salida de requisición: 1699308978</td>
+          <td>1</td>
+          <td>0</td>
+          <td>0</td>
+          <td>3</td>
+          <td>2.50</td>
+          <td>22</td>
         </tr>
       </tbody>
     </table>
   </div><br>
   <div>
-    <table width="400" align="center" border="1" style="color:#00000;font-size:120%;">
+    <table width="200" align="center"border="1" class="table_inf">
       <tr>
-        <td colspan="3" align="center">FUENTE DE FINANCIAMIENTO</td>
+        <td colspan="3" align="center" ><b>FUENTE DE FINANCIAMIENTO</b></td>
       </tr>
-
-      <tr>
-        <td>FONDOS PROPIOS</td>
-        <td>1</td>
-      </tr>
-      <tr>
-        <td>FONDO FONDES</td>
-        <td>2</td>
+      <tr >
+        <td>FONDOS PROPIOS: </td>
+        <td align="center">1</td>
       </tr>
       <tr>
-        <td>DONATIVOS</td>
-        <td>3</td>
+        <td>FONDO FONDES: </td>
+        <td align="center">2</td>
+      </tr>
+      <tr>
+        <td>DONATIVOS: </td>
+        <td align="center">3</td>
       </tr>
     </table>
   </div><br><br><br><br>
@@ -83,7 +110,6 @@
   </form>
   <p>&nbsp;</p>
 </body>
-
 </html>
 
 <script language="javascript">
