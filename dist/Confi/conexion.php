c2e2deea96806fbcf714f0d01c26cb31e2a3fd01
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-12-06 09:52:02 -0600

    actualizando credenciales

--- dist/Confi/conexion.php
+++ dist/Confi/conexion.php
@@ -1,11 +1,11 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "", "sicafi");
+$conexion = mysqli_connect("https://sicafi-ues.com/", "f296809_root", "*W4zm?Y=k,1K", "f296809_sicafi");
 
  function con(){
-     $server = "localhost";
-     $user = "root";
-     $pass = "";
-     $db = "sicafi";
+     $server = "https://sicafi-ues.com/";
+     $user = "f296809_root";
+     $pass = "*W4zm?Y=k,1K";
+     $db = "f296809_sicafi";
 
      $con = mysqli_connect($server,$user,$pass) or die ("Error a Conectar en la BD".mysqli_connect_error());
      mysqli_select_db($con, $db) or die ("Error a Conectar en la BD".mysqli_connect_error());
