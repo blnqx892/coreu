74bffd7c494f39a3c9c778748ae9d4a1fb34adc3
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-12-06 10:18:32 -0600

    cambios

--- dist/Confi/conexion.php
+++ dist/Confi/conexion.php
@@ -5,7 +5,7 @@
      $pass = "@*eIv6G(w)Hn";
      $db = "f296809_sicafi";
 
-     $con = mysqli_connect($server,$user,$pass) or die ("Error a Conectar en la BD".mysqli_connect_error());
+     $con = mysqli_connect($server,$user,$pass) or die ("Error a Conectar en la BD ".mysqli_connect_error());
      mysqli_select_db($con, $db) or die ("Error a Conectar en la BD".mysqli_connect_error());
      return $con;
 }
