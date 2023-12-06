34b1b6e63bb08d7b929f86efbd1aad11bf2b277e
Blanqui <40902380+blnqx892@users.noreply.github.com>
2023-12-06 10:48:37 -0600

    actualizacion

--- dist/menu/hori.php
+++ dist/menu/hori.php
@@ -6,7 +6,7 @@
     </svg>
   </button><a class="header-brand d-md-none" href="#">
     <svg width="118" height="46" alt="CoreUI Logo">
-      <use xlink:href="assets/brand/coreui.svg#full"></use>
+      <use xlink:href="/assets/brand/coreui.svg#full"></use>
     </svg></a>
   <!-- Lógica para notificaciones -->
   <?php
@@ -56,7 +56,7 @@
     <li class="nav-item dropdown d-md-down-none position-relative">
       <a class="nav-link" data-coreui-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
         <svg class="icon icon-lg my-1 mx-2">
-          <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
+          <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
         </svg>
         <?php if($total_stock > 0): ?>
           <span class="badge rounded-pill position-absolute top-0 end-0 bg-danger">
@@ -122,7 +122,7 @@
   <ul class="header-nav ms-3">
     <li class="nav-item dropdown"><a class="nav-link py-0" data-coreui-toggle="dropdown" href="#" role="button"
         aria-haspopup="true" aria-expanded="false">
-        <div class="avatar avatar-md"><img class="avatar-img" src="img/usua.png" alt="user@email.com">
+        <div class="avatar avatar-md"><img class="avatar-img" src="/img/usua.png" alt="user@email.com">
         </div>
       </a>
       <div class="dropdown-menu dropdown-menu-end pt-0">
@@ -130,12 +130,12 @@
           <div class="fw-semibold">Usuario</div>
         </div><a class="dropdown-item" href="#">
           <svg class="icon me-2">
-            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
+            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-user"></use>
           </svg><?php echo $usuario['nombre'] . ' ' . $usuario['apellido'];;?></a><a class="dropdown-item" href="#">
         <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="cerrar.php">
           <svg class="icon me-2">
-            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
+            <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
           </svg> Cerrar Sesión</a>
       </div>
     </li>
--- dist/menu/sidebar.php
+++ dist/menu/sidebar.php
@@ -2,10 +2,10 @@
 <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
   <div class="sidebar-brand d-none d-md-flex">
     <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-      <use xlink:href="assets/brand/coreui.svg#full"></use>
+      <use xlink:href="/assets/brand/coreui.svg#full"></use>
     </svg>
     <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
-      <use xlink:href="assets/brand/coreui.svg#signet"></use>
+      <use xlink:href="/assets/brand/coreui.svg#signet"></use>
     </svg>
   </div>
   <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
@@ -13,7 +13,7 @@
     <li class="nav-group">
       <a class="nav-link nav-group-toggle" href="#">
         <svg class="nav-icon">
-          <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
+          <use xlink:href="/vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
         </svg>Control Adquisicion</a>
       <ul class="nav-group-items">
         <li class="nav-item"><a class="nav-link" href="IngresoEntradas.php"><span class="nav-icon"></span>Ingreso de
@@ -52,7 +52,7 @@
         <li class="nav-item"><a class="nav-link" href="Mprestamo.php"><span class="nav-icon"></span>
             Mantenimiento</a></li>
         <li class="nav-item"><a class="nav-link" href="Mdescargo.php"><span class="nav-icon"></span>
-            Descargo</a></li>  
+            Descargo</a></li>
       </ul>
       <ul class="nav-group-items">
     <li class="nav-item"><a class="nav-link" href="TablaMovi.php"><span class="nav-icon"></span>
