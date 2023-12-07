535222ad74f8191ac4cef9868edbe3aa74bfac47
Natalie <46360156+natalie72@users.noreply.github.com>
2023-12-07 00:01:38 -0600

    nueva base

--- dist/Confi/conexion.php
+++ dist/Confi/conexion.php
@@ -9,7 +9,7 @@ function con(){
 $server = "localhost";
 $user = "f296809_usuario";
 $pass = "ixd5EDM&z8UN";
-$db = "f296809_sicafi";
+$db = "f296809_sicafi_v2";
 
    $con = mysqli_connect($server, $user, $pass) or die ("Error a Conectar en la BD".mysqli_connect_error());
    mysqli_select_db($con, $db) or die ("Error a Conectar en la BD".mysqli_connect_error());
--- dist/Controlador/Entradas/insertEntradas.php
+++ dist/Controlador/Entradas/insertEntradas.php
@@ -29,36 +29,36 @@
     $capaci = intval($_POST["capa"]);
     $x = $_POST["bandera"]; //fk_ingreso_entrada 1
 
-    //if (validacionSql("SELECT VALIDAR('VALIDAR_NUMERO_FACTURA', '$factura') AS resultado")) {
-    //    // Mostrar mensaje de advertencia si el código ya existe
-    //    warningJSON('El Número de factura ya existe.');
-    //    return;
-    //}
+    if (validacionSql("SELECT VALIDAR('VALIDAR_NUMERO_FACTURA', '$factura') AS resultado")) {
+        // Mostrar mensaje de advertencia si el código ya existe
+        warningJSON('El Número de factura ya existe.');
+        return;
+    }else{
     
-    // Establecer una nueva conexión para el procedimiento almacenado
-    $conexion = con();
+        // Establecer una nueva conexión para el procedimiento almacenado
+        $conexion = con();
 
-   $sql = "INSERT INTO ingreso_entradas (fecha_adquisicion,numero_factura,costo_adquisicion,nombre_adquisicion,
-    serie_adquisicion,marca,modelo,color,descripcion_adquisicion,cargo,valor_rescate,fk_categoria,fk_proveedores,numero_motor,
-    numero_chasis,numero_placa,capacidad,boolean_transporte) VALUES
-    ('$fecha','$factura', '$costo','$nombre','$serie','$marca','$modelo','$color','$descrip','$cargo',
-    '$vida','$cate','$prov','$numerom','$numerocha','$numerop','$capaci', $x)";
+        $sql = "INSERT INTO ingreso_entradas (fecha_adquisicion,numero_factura,costo_adquisicion,nombre_adquisicion,
+            serie_adquisicion,marca,modelo,color,descripcion_adquisicion,cargo,valor_rescate,fk_categoria,fk_proveedores,numero_motor,
+            numero_chasis,numero_placa,capacidad,boolean_transporte) VALUES
+            ('$fecha','$factura', '$costo','$nombre','$serie','$marca','$modelo','$color','$descrip','$cargo',
+            '$vida','$cate','$prov','$numerom','$numerocha','$numerop','$capaci', $x)";
 
-    //echo $sql;
-    try {
-        // Ejecutar el procedimiento almacenado
-        $resultado = mysqli_query($conexion, $sql);
-        echo $sql;
-        echo $resultado;
-        // Mostrar mensaje de éxito
-        successJSON('Registro guardado con éxito.');
-    } catch (Exception $e) {
-        // Manejar excepciones durante la ejecución del procedimiento almacenado
-        dangerJSON($e);
-    } finally {
-        // Cerrar la conexión después de ejecutar el procedimiento almacenado
-        mysqli_close($conexion);
+        //echo $sql;
+        try {
+            // Ejecutar el procedimiento almacenado
+            $resultado = mysqli_query($conexion, $sql);
+            echo $sql;
+            echo $resultado;
+            // Mostrar mensaje de éxito
+            successJSON('Registro guardado con éxito.');
+        } catch (Exception $e) {
+            // Manejar excepciones durante la ejecución del procedimiento almacenado
+            dangerJSON($e);
+        } finally {
+            // Cerrar la conexión después de ejecutar el procedimiento almacenado
+            mysqli_close($conexion);
+        }
     }
-
 
 ?>
